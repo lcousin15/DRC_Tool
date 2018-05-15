434976a2599855fb506a7d19711318cfae7f2487
lcousin15 <loic.cousin@ksilink.com>
2018-05-15 14:53:15 +0200

    list of compound contains DMSO or unthreated

--- DRC/MainTab.cs
+++ DRC/MainTab.cs
@@ -696,7 +696,7 @@ namespace DRC
                 {
                     string cpd_id = list_cpd[idx].ToString();
 
-                    if (cpd_id == "DMSO" || cpd_id == "Untreated")
+                    if (cpd_id.Contains("DMSO") || cpd_id.Contains("Untreated"))
                         continue;
 
                     List<Chart_DRC> list_chart = descriptors_chart[cpd_id];
